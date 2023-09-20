85613ec098f2844e7648d6097ec8c83ebd05db49
Yash Sharma <yash27sharma@gmail.com>
2023-09-19 20:56:37 -0400

    html strcture

---
+++ src/lib/todo-item.svelte
@@ -0,0 +1,15 @@
+<div>
+    <form method="" action="">
+        <input type="hidden" name="done" value="" />
+        <button aria-label="Mark done/not Done">Done/NotDone</button>
+    </form>
+
+    <form method="" action="">
+        <input type="text"/>
+        <button aria-label="Save todo">Save</button>
+    </form>
+
+    <form method="" action="">
+        <button aria-label="Delete todo">Delete</button>
+    </form>
+</div>
